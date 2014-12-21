dfd032afd6eaddd9ee30ed4cbfb93e247be781b6
Gerry Hall <gerry.j.hall@gmail.com>
2014-12-21 18:38:24 -0500

    add documentation to interfaces

--- PluginCalculator.Core/Repositories/Math/IMathRepository.cs
+++ PluginCalculator.Core/Repositories/Math/IMathRepository.cs
@@ -3,7 +3,23 @@ using System;
 namespace PluginCalculator.Core.Repositories.Math
 {
 	public interface IMathRepository {
+
+		/// <summary>
+		/// Takes the first part of an equation, the operator, and the second part, combines and returns the equation in string format using +,-,/,* operators.
+		/// </summary>
+		/// <returns>The equation.</returns>
+		/// <param name="first">First part of the equation</param>
+		/// <param name="operation">Operation to be performed</param>
+		/// <param name="second">Second part of the equation</param>
 		string CreateEquation(string first, MathOperation operation, string second);
+
+		/// <summary>
+		/// Determines whether this instance is valid for the specified equation parts.
+		/// </summary>
+		/// <returns><c>Valid</c> if valid, <c>DivisionByZero</c> if attempting to perform division by zero.</returns>
+		/// <param name="first">First.</param>
+		/// <param name="operation">Operation.</param>
+		/// <param name="second">Second.</param>
 		EquationValidity IsValid(string first, MathOperation operation, string second);
 	}	
 }
--- PluginCalculator.Core/Repositories/Result/IResultRepository.cs
+++ PluginCalculator.Core/Repositories/Result/IResultRepository.cs
@@ -4,6 +4,10 @@ namespace PluginCalculator.Core.Repositories.Result
 {
 	public interface IResultRepository
 	{
+		/// <summary>
+		/// Execute the specified mathString to get the result. Must use *,/,+,- operators.
+		/// </summary>
+		/// <param name="mathString">Math string.</param>
 		string Execute(string mathString);
 	}
 }
