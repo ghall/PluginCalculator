2a14b38e172f82c27dd6c11e029fc4fa7e3173a1
Gerry Hall <gerry.j.hall@gmail.com>
2014-12-21 13:12:13 -0500

    add unit tests and start to build out view model

---
+++ PluginCalculator.Core.UnitTests/ViewModels/CalculatorViewModelTests.cs
@@ -0,0 +1,273 @@
+﻿using System;
+using NUnit.Framework;
+using PluginCalculator.Core.ViewModels;
+using Cirrious.MvvmCross.Test.Core;
+
+namespace PluginCalculator.Core.UnitTests.ViewModels
+{
+	[TestFixture]
+	public class CalculatorViewModelTests : MvxIoCSupportingTest
+	{
+		private CalculatorViewModel _target;
+
+		[SetUp]
+		public void Setup() {
+			base.Setup ();
+
+			_target = new CalculatorViewModel ();
+		}
+
+		[Test]
+		public void ResultField_TestProperty() {
+			Assume.That (_target.ResultField, Is.EqualTo("0"));
+
+			_target.ResultField = "7";
+
+			Assert.AreEqual ("7", _target.ResultField);
+		}
+
+		[Test]
+		public void ZeroPressed_Success() {
+			_target.ResultField = "1";
+
+			_target.ZeroPressed.Execute ();
+
+			Assert.AreEqual ("10", _target.ResultField);
+		}
+
+		[Test]
+		public void ZeroPressed_NoDoubleZeros() {
+			_target.ResultField = "0";
+
+			_target.ZeroPressed.Execute ();
+
+			Assert.AreEqual ("0", _target.ResultField);
+		}
+
+		[Test]
+		public void OnePressed_Success() {
+			_target.OnePressed.Execute ();
+
+			Assert.AreEqual ("1", _target.ResultField);
+		}
+
+		[Test]
+		public void OnePressed_Success_Append() {
+			_target.ResultField = "1";
+
+			_target.OnePressed.Execute ();
+
+			Assert.AreEqual ("11", _target.ResultField);
+		}
+
+		[Test]
+		public void OnePressed_NoLeadingZeros() {
+			_target.ResultField = "0";
+
+			_target.OnePressed.Execute ();
+
+			Assert.AreEqual ("1", _target.ResultField);
+		}
+
+		[Test]
+		public void TwoPressed_Success() {
+			_target.TwoPressed.Execute ();
+
+			Assert.AreEqual ("2", _target.ResultField);
+		}
+
+		[Test]
+		public void TwoPressed_Success_Append() {
+			_target.ResultField = "1";
+
+			_target.TwoPressed.Execute ();
+
+			Assert.AreEqual ("12", _target.ResultField);
+		}
+
+		[Test]
+		public void TwoPressed_NoLeadingZeros() {
+			_target.ResultField = "0";
+
+			_target.TwoPressed.Execute ();
+
+			Assert.AreEqual ("2", _target.ResultField);
+		}
+
+		[Test]
+		public void ThreePressed_Success() {
+			_target.ThreePressed.Execute ();
+
+			Assert.AreEqual ("3", _target.ResultField);
+		}
+
+		[Test]
+		public void ThreePressed_Success_Append() {
+			_target.ResultField = "1";
+
+			_target.ThreePressed.Execute ();
+
+			Assert.AreEqual ("13", _target.ResultField);
+		}
+
+		[Test]
+		public void ThreePressed_NoLeadingZeros() {
+			_target.ResultField = "0";
+
+			_target.ThreePressed.Execute ();
+
+			Assert.AreEqual ("3", _target.ResultField);
+		}
+
+		[Test]
+		public void FourPressed_Success() {
+			_target.FourPressed.Execute ();
+
+			Assert.AreEqual ("4", _target.ResultField);
+		}
+
+		[Test]
+		public void FourPressed_Success_Append() {
+			_target.ResultField = "1";
+
+			_target.FourPressed.Execute ();
+
+			Assert.AreEqual ("14", _target.ResultField);
+		}
+
+		[Test]
+		public void FourPressed_NoLeadingZeros() {
+			_target.ResultField = "0";
+
+			_target.FourPressed.Execute ();
+
+			Assert.AreEqual ("4", _target.ResultField);
+		}
+
+		[Test]
+		public void FivePressed_Success() {
+			_target.FivePressed.Execute ();
+
+			Assert.AreEqual ("5", _target.ResultField);
+		}
+
+		[Test]
+		public void FivePressed_Success_Append() {
+			_target.ResultField = "1";
+
+			_target.FivePressed.Execute ();
+
+			Assert.AreEqual ("15", _target.ResultField);
+		}
+
+		[Test]
+		public void FivePressed_NoLeadingZeros() {
+			_target.ResultField = "0";
+
+			_target.FivePressed.Execute ();
+
+			Assert.AreEqual ("5", _target.ResultField);
+		}
+
+		[Test]
+		public void SixPressed_Success() {
+			_target.SixPressed.Execute ();
+
+			Assert.AreEqual ("6", _target.ResultField);
+		}
+
+		[Test]
+		public void SixPressed_Success_Append() {
+			_target.ResultField = "1";
+
+			_target.SixPressed.Execute ();
+
+			Assert.AreEqual ("16", _target.ResultField);
+		}
+
+		[Test]
+		public void SixPressed_NoLeadingZeros() {
+			_target.ResultField = "0";
+
+			_target.SixPressed.Execute ();
+
+			Assert.AreEqual ("6", _target.ResultField);
+		}
+
+		[Test]
+		public void SevenPressed_Success() {
+			_target.SevenPressed.Execute ();
+
+			Assert.AreEqual ("7", _target.ResultField);
+		}
+
+		[Test]
+		public void SevenPressed_Success_Append() {
+			_target.ResultField = "1";
+
+			_target.SevenPressed.Execute ();
+
+			Assert.AreEqual ("17", _target.ResultField);
+		}
+
+		[Test]
+		public void SevenPressed_NoLeadingZeros() {
+			_target.ResultField = "0";
+
+			_target.SevenPressed.Execute ();
+
+			Assert.AreEqual ("7", _target.ResultField);
+		}
+
+		[Test]
+		public void EightPressed_Success() {
+			_target.EightPressed.Execute ();
+
+			Assert.AreEqual ("8", _target.ResultField);
+		}
+
+		[Test]
+		public void EightPressed_Success_Append() {
+			_target.ResultField = "1";
+
+			_target.EightPressed.Execute ();
+
+			Assert.AreEqual ("18", _target.ResultField);
+		}
+
+		[Test]
+		public void EightPressed_NoLeadingZeros() {
+			_target.ResultField = "0";
+
+			_target.EightPressed.Execute ();
+
+			Assert.AreEqual ("8", _target.ResultField);
+		}
+
+		[Test]
+		public void NinePressed_Success() {
+			_target.NinePressed.Execute ();
+
+			Assert.AreEqual ("9", _target.ResultField);
+		}
+
+		[Test]
+		public void NinePressed_Success_Append() {
+			_target.ResultField = "1";
+
+			_target.NinePressed.Execute ();
+
+			Assert.AreEqual ("19", _target.ResultField);
+		}
+
+		[Test]
+		public void NinePressed_NoLeadingZeros() {
+			_target.ResultField = "0";
+
+			_target.NinePressed.Execute ();
+
+			Assert.AreEqual ("9", _target.ResultField);
+		}
+	}
+}
+
--- PluginCalculator.Core/ViewModels/CalculatorViewModel.cs
+++ PluginCalculator.Core/ViewModels/CalculatorViewModel.cs
@@ -5,8 +5,115 @@ namespace PluginCalculator.Core.ViewModels
 {
 	public class CalculatorViewModel : MvxViewModel
 	{
+		private string _resultField;
+		private IMvxCommand _zeroPressed;
+		private IMvxCommand _onePressed;
+		private IMvxCommand _twoPressed;
+		private IMvxCommand _threePressed;
+		private IMvxCommand _fourPressed;
+		private IMvxCommand _fivePressed;
+		private IMvxCommand _sixPressed;
+		private IMvxCommand _sevenPressed;
+		private IMvxCommand _eightPressed;
+		private IMvxCommand _ninePressed;
+
 		public CalculatorViewModel ()
 		{
+		}
+
+		public string ResultField {
+			get { return _resultField ?? "0"; }
+			set { 
+				_resultField = value;
+				RaisePropertyChanged (ResultField);
+			}
+		}
+
+		public IMvxCommand ZeroPressed {
+			get { return _zeroPressed ?? (_zeroPressed = new MvxCommand(DoZeroPressed)); }
+		}
+
+		public IMvxCommand OnePressed {
+			get { return _onePressed ?? (_onePressed = new MvxCommand(DoOnePressed)); }
+		}
+
+		public IMvxCommand TwoPressed {
+			get { return _twoPressed ?? (_twoPressed = new MvxCommand(DoTwoPressed)); }
+		}
+
+		public IMvxCommand ThreePressed {
+			get { return _threePressed ?? (_threePressed = new MvxCommand(DoThreePressed)); }
+		}
+
+		public IMvxCommand FourPressed {
+			get { return _fourPressed ?? (_fourPressed = new MvxCommand(DoFourPressed)); }
+		}
+
+		public IMvxCommand FivePressed {
+			get { return _fivePressed ?? (_fivePressed = new MvxCommand(DoFivePressed)); }
+		}
+
+		public IMvxCommand SixPressed {
+			get { return _sixPressed ?? (_sixPressed = new MvxCommand(DoSixPressed)); }
+		}
+
+		public IMvxCommand SevenPressed {
+			get { return _sevenPressed ?? (_sevenPressed = new MvxCommand(DoSevenPressed)); }
+		}
+
+		public IMvxCommand EightPressed {
+			get { return _eightPressed ?? (_eightPressed = new MvxCommand(DoEightPressed)); }
+		}
+
+		public IMvxCommand NinePressed {
+			get { return _ninePressed ?? (_ninePressed = new MvxCommand(DoNinePressed)); }
+		}
+			
+		private void NumberPressed(int number) {
+			if ("0" == ResultField)
+				ResultField = string.Empty;
+
+			ResultField += number.ToString();
+		}
+
+		private void DoZeroPressed() {
+			NumberPressed (0);
+		}
+
+		private void DoOnePressed() {
+			NumberPressed (1);
+		}
+
+		private void DoTwoPressed() {
+			NumberPressed (2);
+		}
+
+		private void DoThreePressed() {
+			NumberPressed (3);
+		}
+
+		private void DoFourPressed() {
+			NumberPressed (4);
+		}
+
+		private void DoFivePressed() {
+			NumberPressed (5);
+		}
+
+		private void DoSixPressed() {
+			NumberPressed (6);
+		}
+
+		private void DoSevenPressed() {
+			NumberPressed (7);
+		}
+
+		private void DoEightPressed() {
+			NumberPressed (8);
+		}
+
+		private void DoNinePressed() {
+			NumberPressed (9);
 		}
 	}
 }
